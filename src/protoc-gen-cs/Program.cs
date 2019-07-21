f2174fc3e4f2788cd0b2d51c5938e6853b30774e
Sourabh Shirhatti <shirhatti@outlook.com>
2019-07-21 00:55:30 -0700

    Add protoc-gen-cs

---
+++ src/protoc-gen-cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using Google.ProtocolBuffers;
+using Google.ProtocolBuffers.Compiler.PluginProto;
+using Google.ProtocolBuffers.DescriptorProtos;
+using System;
+using System.Diagnostics;
+
+namespace Protogen
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            DescriptorProtoFile.Descriptor.ToString();
+            ExtensionRegistry extensionRegistry = ExtensionRegistry.CreateInstance();
+            CSharpOptions.RegisterAllExtensions(extensionRegistry);
+
+            CodeGeneratorRequest request;
+            var response = new CodeGeneratorResponse.Builder();
+            try
+            {
+                using var input = Console.OpenStandardInput();
+                request = CodeGeneratorRequest.ParseFrom(input, extensionRegistry);
+            }
+            catch (Exception e)
+            {
+                response.Error += e.ToString();
+            }
+
+            return 0;
+        }
+    }
+}
