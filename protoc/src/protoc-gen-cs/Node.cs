57fe07f3929acd404ef4aa4765962f254517a58f
John Luo <jt.luo@mail.utoronto.ca>
2019-07-23 12:25:55 -0700

    Building AST (but some nodes are not correct)

---
+++ protoc/src/protoc-gen-cs/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Protogen
+{
+    public class Node
+    {
+        public Node Parent { get; set; }
+        public NodeInfo Info { get; set; }
+        public List<Node> Children { get; } = new List<Node>();
+
+        public Node GetChildNodeAt(int line, int col)
+        {
+            return Children
+                .SingleOrDefault(n => n.Info.StartLine <= line && line <= n.Info.EndLine && n.Info.StartCol <= col && col <= n.Info.EndCol)
+                ?.GetChildNodeAt(line, col)
+                ?? this;
+        }
+    }
+}
--- protoc/src/protoc-gen-cs/Program.cs
+++ protoc/src/protoc-gen-cs/Program.cs
@@ -3,104 +3,17 @@ using Google.ProtocolBuffers.Compiler.PluginProto;
 using Google.ProtocolBuffers.DescriptorProtos;
 using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Text.Json;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using Google.ProtocolBuffers.Descriptors;
-using System.Collections;
 using System.IO;
 using System.Text;
 
 namespace Protogen
 {
-    class Info
-    {
-        public int StartLine { get; set; }
-        public int StartCol { get; set; }
-        public int EndLine { get; set; }
-        public int EndCol { get; set; }
-        public string Type { get; set; }
-        public string Content { get; set; }
-        public string File { get; set; }
-
-        public void ResolveContent(string[] lines)
-        {
-            if (StartLine == EndLine)
-            {
-                Content = lines[StartLine].Substring(StartCol, EndCol - StartCol);
-            }
-            else
-            {
-                var builder = new StringBuilder();
-                builder.Append(lines[StartLine].Substring(StartCol));
-                for (int i = StartLine + 1; i < EndLine; i++)
-                {
-                    builder.Append(lines[i]);
-                }
-                builder.Append(lines[EndLine].Substring(0, EndCol));
-                Content = builder.ToString();
-            }
-        }
-    }
 
     public class Program
     {
-        private static List<Type> DescriptorProtos = new List<Type>
-        {
-            typeof(FileDescriptorProto),
-            typeof(ServiceDescriptorProto),
-            typeof(EnumDescriptorProto),
-            typeof(FileDescriptorProto),
-            typeof(FieldDescriptorProto),
-        };
-
-        private static string ResolveType(IList<int> pathList)
-        {
-            if (pathList.Count == 0)
-            {
-                return "root";
-            }
-
-            var resolvedMessageDescriptor = FileDescriptorProto.Descriptor;
-            var resolvedTypeName= "";
-
-            foreach (var path in pathList)
-            {
-                if (path == 0)
-                {
-                    continue;
-                }
-
-                // new in proto3
-                if (path == 12)
-                {
-                    return "syntax";
-                }
-
-                var field = resolvedMessageDescriptor.FindFieldByNumber(path);
-
-                if (field == null)
-                {
-                    continue;
-                }
-
-                resolvedTypeName = field.Name;
-                var matchingDescriptorProto = DescriptorProtos.SingleOrDefault(t => t.Name.StartsWith(resolvedTypeName, StringComparison.OrdinalIgnoreCase));
-
-                if (matchingDescriptorProto == null)
-                {
-                    continue;
-                }
-
-                var messageDescriptor = matchingDescriptorProto.GetProperty("Descriptor").GetValue(null);
-                resolvedMessageDescriptor = (MessageDescriptor)messageDescriptor;
-            }
-
-            return resolvedTypeName;
-        }
 
         public static int Main(string[] args)
         {
@@ -110,31 +23,58 @@ namespace Protogen
 
             CodeGeneratorRequest request;
             var response = new CodeGeneratorResponse.Builder();
+            var infoList = new List<NodeInfo>();
             try
             {
                 using var input = Console.OpenStandardInput();
                 request = CodeGeneratorRequest.ParseFrom(input, extensionRegistry);
-                var lines = File.ReadAllLines("D:\\protobuf-langserver\\protoc\\greet.proto");
-                var infoList = new List<Info>();
+                var lines = File.ReadAllLines("C:\\gh\\protobuf-langserver\\protoc\\greet.proto");
 
                 Debugger.Launch();
                 foreach (var file in request.ProtoFileList)
                 {
+                    Node ast = null;
+
                     foreach (var location in file.SourceCodeInfo.LocationList)
                     {
-                        var info = new Info
+                        var info = new NodeInfo
                         {
                             StartLine = location.SpanList[0],
                             StartCol = location.SpanList[1],
                             EndLine = location.SpanCount == 3 ? location.SpanList[0] : location.SpanList[2],
                             EndCol = location.SpanCount == 3 ? location.SpanList[2] : location.SpanList[3],
-                            File = file.Name,
-                            Type = ResolveType(location.PathList)
+                            File = file.Name
                         };
 
+                        info.ResolvePath(location.PathList);
                         info.ResolveContent(lines);
 
+                        if (info.Type == "label")
+                        {
+                            // There's a bug in protobuf where labels in version 3 isn't parsed correctly
+                            info.EndLine = info.StartLine;
+                            info.EndCol = lines[info.StartLine].Length - 1;
+                        }
+
                         infoList.Add(info);
+                    }
+
+                    foreach (var info in infoList.OrderByDescending(i => i.Content.Length))
+                    {
+
+                        if (ast == null)
+                        {
+                            ast = new Node { Info = info };
+                        }
+                        else
+                        {
+                            var node = ast.GetChildNodeAt(info.StartLine, info.StartCol);
+                            node.Children.Add(new Node
+                            {
+                                Info = info,
+                                Parent = node
+                            });
+                        }
                     }
                 }
             }
@@ -143,7 +83,7 @@ namespace Protogen
                 response.Error += e.ToString();
             }
 
-            return 0;
+                return 0;
         }
     }
 }
